b6faccf823b9df55bb6afb60ca92f0ce5072e247
hoanglinh371 <hoanglinh371.utc@gmail.com>
2023-10-07 00:27:14 +0700

    get configs api

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 import App from './App.jsx';
+import ConfigProvider from './contexts/config.context';
 import UserProvider from './contexts/user.context';
+
 import './index.css';
 
 const queryClient = new QueryClient();
@@ -12,9 +14,11 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <UserProvider>
-        <App />
-      </UserProvider>
+      <ConfigProvider>
+        <UserProvider>
+          <App />
+        </UserProvider>
+      </ConfigProvider>
       <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>,
